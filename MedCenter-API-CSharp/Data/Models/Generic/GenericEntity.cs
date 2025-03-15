d35360848c8a2d40ffe921bcbde60d6659a32902
mo1ty <fire_4u@protonmail.com>
2025-03-15 14:47:39 +0100

    Updated classes to stop throwing context issue
    Signed-off-by: mo1ty <fire_4u@protonmail.com>

--- MedCenter-API-CSharp/Data/Models/Generic/GenericEntity.cs
+++ MedCenter-API-CSharp/Data/Models/Generic/GenericEntity.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MedCenter_API_CSharp.Models.Generic;
 
 public abstract class GenericEntity
 {
-    public long Id { get; set; }
+    [Key] public long Id { get; set; }
 }
